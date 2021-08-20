e27b44ada708c367f4a8c486b90e54510fc941cc
tanto untung <untung2k3@yahoo.com>
2021-08-20 16:49:45 +0200

    bug in details contract, drop down still null and for add contract, infinite loop

--- src/components/contracts/ContractTable.jsx
+++ src/components/contracts/ContractTable.jsx
@@ -4,12 +4,12 @@ const ContractTable = (props) => {
   const rows = props.contracts.map((contract) => {
     return (
       <tr key={contract.id}>
+        <td>{contract.roomInQuestion.roomNumber}</td>  
+        <td>{contract.tenantInQuestion.tenantName}</td>  
         <td>{contract.roomPrice}</td>
         {/* <td>{contract.paymentDate}</td>   
         <td>{contract.startDate}</td>  
         <td>{contract.endDate}</td>   */}
-        <td>{contract.roomInQuestionId}</td>  
-        <td>{contract.tenantInQuestionId}</td>  
            
         <td
           className="btn btn-info"
@@ -27,12 +27,12 @@ const ContractTable = (props) => {
       <table className="table table-striped table-bordered">
         <thead className="thead-dark">
           <tr>
+            <th>Room In Question</th> 
+            <th>Tenant In Question</th>                     
             <th>Room Price</th>
             {/* <th>Payment Date</th> 
             <th>Start Date</th> 
             <th>End Date</th>  */}
-            <th>Room In Question</th> 
-            <th>Tenant In Question</th>                     
             <th>Actions</th>
           </tr>
         </thead>
